33b7919949f1b7d80b05769cc05a6ca5ae0360b8
n3s4r <141773812+n3s4r@users.noreply.github.com>
2025-12-26 21:48:24 +0600

    Create exports.dart

---
+++ lib/core/exports.dart
@@ -0,0 +1,25 @@
+/// Core exports for easy importing
+library core;
+
+// Constants
+export 'constants/app_constants.dart';
+
+// Error handling
+export 'error/exceptions.dart';
+export 'error/failures.dart';
+
+// Network
+export 'network/supabase_client.dart';
+
+// Theme
+export 'theme/app_theme.dart';
+
+// Utils
+export 'utils/supabase_error_handler.dart';
+export 'utils/typedefs.dart';
+
+// Widgets
+export 'widgets/gradient_button.dart';
+export 'widgets/interest_chip.dart';
+export 'widgets/profile_avatar.dart';
+
